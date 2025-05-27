8fa7744391e2b2cc1eba9d990d9af50eb7ec5d5d
Uzay Kuzey <114340199+uzaykuzey@users.noreply.github.com>
2025-05-28 01:09:56 +0300

    refactor: separate miRNA controller and service classes

--- src/main/java/com/bioinformatics/bioinformatics/controller/MiRNAController.java
+++ src/main/java/com/bioinformatics/bioinformatics/controller/MiRNAController.java
@@ -1,38 +1,22 @@
 package com.bioinformatics.bioinformatics.controller;
 
-import com.bioinformatics.bioinformatics.model.GenePredictionDTO;
-import com.bioinformatics.bioinformatics.model.MiRNA;
 import com.bioinformatics.bioinformatics.model.Prediction;
 import com.bioinformatics.bioinformatics.model.Search;
-import com.bioinformatics.bioinformatics.repository.MiRNARepository;
+import com.bioinformatics.bioinformatics.service.MiRNAService;
 import com.bioinformatics.bioinformatics.service.PastSearchesService;
-import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class MiRNAController {
-
     @Autowired
-    private MiRNARepository miRNARepository;
-
+    private MiRNAService miRNAService;
     @Autowired
     private PastSearchesService pastSearchesService;
-
-
 
     /**
      * @param mirnaNames List of microRNA names to query.
@@ -48,75 +32,8 @@ public class MiRNAController {
             @RequestParam("toolSelection") String toolSelection,
             @RequestParam("heuristic") String heuristic) {
 
-        long startTime = System.nanoTime();
-        var rawPredictions = miRNARepository.getPredictions(
-                                            List.of(mirnaNames),
-                                            List.of(tools),
-                                            toolSelection,
-                                            heuristic);
-
-        int geneCount;
-        int pathwayCount = 0;
-
-        ArrayList<Prediction.PredictionValues> predictionValues = new ArrayList<>();
-        if(rawPredictions==null || rawPredictions.isEmpty())
-        {
-            predictionValues = new ArrayList<>();
-            geneCount = 0;
-        }
-        else
-        {
-            for(var predictionDTO : rawPredictions) {
-                pathwayCount += predictionDTO.pathways().size();
-                predictionValues.add(new Prediction.PredictionValues(predictionDTO.gene(), predictionDTO.tools().toArray(new String[0]), predictionDTO.pathways().toArray(new String[0])));
-            }
-            geneCount = predictionValues.size();
-        }
-
-        long durationInNanoSeconds = (System.nanoTime() - startTime);
-
-        Prediction prediction = new Prediction(mirnaNames, predictionValues.toArray(Prediction.PredictionValues[]::new),
-                durationToString(durationInNanoSeconds), geneCount, pathwayCount);
-
-        System.out.println(prediction.getSearchTime());
-
         pastSearchesService.saveSearchAsync(new Search(mirnaNames, tools, toolSelection, heuristic));
-
-        return ResponseEntity.ok(prediction);
-    }
-
-    private String durationToString(long durationInNanoSeconds) {
-        double actualDuration;
-        String durationUnit;
-
-        if(durationInNanoSeconds <= 0) {
-            return "0 ns";
-        }
-
-        int exponent = (int) Math.floor(Math.log10(durationInNanoSeconds));
-
-        if(exponent>5)
-        {
-            actualDuration = durationInNanoSeconds / 1000000000d;
-            durationUnit = "s";
-        }
-        else if(exponent>2)
-        {
-            actualDuration = durationInNanoSeconds / 1000000d;
-            durationUnit = "ms";
-        }
-        else if(exponent>0)
-        {
-            actualDuration = durationInNanoSeconds / 1000d;
-            durationUnit = "μs";
-        }
-        else
-        {
-            actualDuration = durationInNanoSeconds;
-            durationUnit = "ns";
-        }
-
-        return (Math.round(actualDuration * 1000)/1000d) + " " + durationUnit;
+        return ResponseEntity.ok(miRNAService.getPredictions(mirnaNames, tools, toolSelection, heuristic));
     }
 
     @GetMapping("/pastSearches")
@@ -124,12 +41,8 @@ public class MiRNAController {
         return ResponseEntity.ok(pastSearchesService.getPastSearches());
     }
 
-
-
     @GetMapping("/pathways")
     public ResponseEntity<List<Map<String, Object>>> getPathwaysByGene(@RequestParam("geneName") String geneName) {
-        List<Map<String, Object>> pathways = miRNARepository.findPathwaysByGeneName(geneName);
-        return ResponseEntity.ok(pathways);
+        return ResponseEntity.ok(miRNAService.getPathwaysByGene(geneName));
     }
-
 }
---
+++ src/main/java/com/bioinformatics/bioinformatics/service/MiRNAService.java
@@ -0,0 +1,89 @@
+package com.bioinformatics.bioinformatics.service;
+
+import com.bioinformatics.bioinformatics.model.Prediction;
+import com.bioinformatics.bioinformatics.repository.MiRNARepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class MiRNAService {
+    @Autowired
+    private MiRNARepository miRNARepository;
+
+    public Prediction getPredictions(String[] mirnaNames, String[] tools, String toolSelection, String heuristic) {
+
+        long startTime = System.nanoTime();
+        var rawPredictions = miRNARepository.getPredictions(
+                List.of(mirnaNames),
+                List.of(tools),
+                toolSelection,
+                heuristic);
+
+        int geneCount;
+        int pathwayCount = 0;
+
+        ArrayList<Prediction.PredictionValues> predictionValues = new ArrayList<>();
+        if(rawPredictions==null || rawPredictions.isEmpty())
+        {
+            predictionValues = new ArrayList<>();
+            geneCount = 0;
+        }
+        else
+        {
+            for(var predictionDTO : rawPredictions) {
+                pathwayCount += predictionDTO.pathways().size();
+                predictionValues.add(new Prediction.PredictionValues(predictionDTO.gene(), predictionDTO.tools().toArray(new String[0]), predictionDTO.pathways().toArray(new String[0])));
+            }
+            geneCount = predictionValues.size();
+        }
+
+        long durationInNanoSeconds = (System.nanoTime() - startTime);
+
+        Prediction prediction = new Prediction(mirnaNames, predictionValues.toArray(Prediction.PredictionValues[]::new),
+                durationToString(durationInNanoSeconds), geneCount, pathwayCount);
+
+        System.out.println(prediction.getSearchTime());
+
+        return prediction;
+    }
+
+    private String durationToString(long durationInNanoSeconds) {
+        double actualDuration;
+        String durationUnit;
+
+        if(durationInNanoSeconds <= 0) {
+            return "0 ns";
+        }
+
+        int exponent = (int) Math.floor(Math.log10(durationInNanoSeconds));
+
+        if(exponent>5)
+        {
+            actualDuration = durationInNanoSeconds / 1000000000d;
+            durationUnit = "s";
+        }
+        else if(exponent>2)
+        {
+            actualDuration = durationInNanoSeconds / 1000000d;
+            durationUnit = "ms";
+        }
+        else if(exponent>0)
+        {
+            actualDuration = durationInNanoSeconds / 1000d;
+            durationUnit = "μs";
+        }
+        else
+        {
+            actualDuration = durationInNanoSeconds;
+            durationUnit = "ns";
+        }
+
+        return (Math.round(actualDuration * 1000)/1000d) + " " + durationUnit;
+    }
+
+    public List<Map<String, Object>> getPathwaysByGene(String geneName) {
+        return miRNARepository.findPathwaysByGeneName(geneName);
+    }
+}
